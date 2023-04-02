940ac6387a16b385fe4f32206ba35436fa2c0572
BasudevBharatBhushan <bharatbhushan173@gmail.com>
2023-04-03 00:13:41 +0530

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,33 @@
 import { useEffect, useMemo, useState } from "react";
-
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import { PhantomWalletAdapter } from "@solana/wallet-adapter-wallets";
+import "@solana/wallet-adapter-ui/styles.css";
 
 import Main from "./Main";
 import { RPC_ENDPOINT } from "../utils";
 
 export default function Home() {
+  const [mounted, setMounted] = useState(false);
+
+  const wallets = useMemo(() => [new PhantomWalletAdapter()], []);
+
+  //In order to fix SSR error with Next JS
+  useEffect(() => {
+    setMounted(true);
+  }, []);
 
   return (
-    <Main />
+    <ConnectionProvider
+      endpoint={RPC_ENDPOINT}
+      config={{ commitment: "confirmed" }}
+    >
+      <WalletProvider wallets={wallets} autoconnect>
+        <WalletModalProvider>{mounted && <Main />}</WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
   );
 }
--- utils/constants.js
+++ utils/constants.js
@@ -1,6 +1,9 @@
 import { PublicKey } from "@solana/web3.js";
 
-export const RPC_ENDPOINT = "https://metaplex.devnet.rpcpool.com/";
+export const RPC_ENDPOINT =
+  "https://light-hardworking-dinghy.solana-devnet.discover.quiknode.pro/932e45579637d0205cfeb3c3831a8b14c9d214b8/";
 
 // Change this with your candy machine id
-export const CANDY_MACHINE_ID = new PublicKey("APDPkmr6dS2E4sCH6jVtFGX7dzuAbVnZkokikN4kyFiU");
+export const CANDY_MACHINE_ID = new PublicKey(
+  "APDPkmr6dS2E4sCH6jVtFGX7dzuAbVnZkokikN4kyFiU"
+);
